ea28a24799e63a90581096a4a94ea6e2a2484ab3
endoverdosing <endoverdosing@gmail.com>
2026-05-29 16:20:56 -0700

    Updated

--- push.bat
+++ push.bat
@@ -17,11 +17,19 @@ git remote add hf3 https://huggingface.co/spaces/MissouriMonster/momo
 git remote remove hf4 2>nul
 git remote add hf4 https://huggingface.co/spaces/MissouriMonster/popr
 
+git remote remove hf5 2>nul
+git remote add hf5 https://huggingface.co/spaces/MissouriMonster/peaktv
+
+git remote remove hf6 2>nul
+git remote add hf6 https://huggingface.co/spaces/MissouriMonster/movieslay
+
 echo Pushing to Hugging Face...
 git push hf main --force
 git push hf2 main --force
 git push hf3 main --force
 git push hf4 main --force
+git push hf5 main --force
+git push hf6 main --force
 
 echo Pushing to GitHub...
 git push github main --force
