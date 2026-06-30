e04887fb223a0b0c62ba4053b79d213ad1c658ca
endoverdosing <endoverdosing@gmail.com>
2026-06-29 21:21:11 -0700

    Updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ import { ensureApiKeysTable, fetchActiveApiKeys } from '../../db.js';
 
 dotenv.config();
 
-const BYPASS_LOCALHOST = true;
+const BYPASS_LOCALHOST = false;
 
 const API_KEYS = new Map();
 const rateLimitMap = new Map();
