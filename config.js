ce01f02a399d10ed5a80f2812af8d1b247dceb5c
EndOverdosing <endoverdosing@gmail.com>
2026-07-02 07:24:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -82,17 +82,6 @@ export const SOURCES = [
     },
 
     {
-        key: 'vidsuper',
-        label: 'VidSuper',
-        sourceFile: 'vidsuper',
-        proxyParam: 'vs',
-        timeout: 30000,
-        jitter: 500,
-        retries: 2,
-        multiUrl: true,
-    },
-
-    {
         key: 'vixsrc',
         label: 'VixSrc',
         sourceFile: 'vixsrc',
