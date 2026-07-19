f107ccf7e6367b7e17611fc9acfb6f1c8f46abcc
endoverdosing <endoverdosing@gmail.com>
2026-07-18 21:30:39 -0700

    Updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ import { ensureApiKeysTable, fetchActiveApiKeys, fetchDisabledApiKeys, ensurePub
 
 dotenv.config();
 
-const BYPASS_AUTH = true;
+const BYPASS_AUTH = false;
 
 const API_KEYS = new Map();
 const DISABLED_KEYS = new Set();
